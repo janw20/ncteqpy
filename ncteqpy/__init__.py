22542ae58d11d570fbf7893822d7b21b86d07dbf
Jan Wissmann <jan.wissmann.jw@gmail.com>
2024-10-21 17:36:51 +0200

    Add Cut and kinematic variables to __init__

--- ncteqpy/__init__.py
+++ ncteqpy/__init__.py
@@ -1,3 +1,5 @@
 from ncteqpy.chi2 import Chi2
+from ncteqpy.cuts import Cut
 from ncteqpy.data import Dataset, Datasets
+from ncteqpy.kinematic_variables import PT, PT_MAX, PT_MIN, Q2, SQRT_S, YMAX, YMIN, X, Y
 from ncteqpy.settings import Settings
