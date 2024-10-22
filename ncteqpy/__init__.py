3c454b19ef5b98822ddd38a0957714edf86bf69f
Jan Wissmann <jan.wissmann.jw@gmail.com>
2024-10-22 14:53:34 +0200

    Rename Y_MIN and Y_MAX

--- ncteqpy/__init__.py
+++ ncteqpy/__init__.py
@@ -1,5 +1,5 @@
 from ncteqpy.chi2 import Chi2
 from ncteqpy.cuts import Cut
 from ncteqpy.data import Dataset, Datasets
-from ncteqpy.kinematic_variables import PT, PT_MAX, PT_MIN, Q2, SQRT_S, YMAX, YMIN, X, Y
+from ncteqpy.kinematic_variables import PT, PT_MAX, PT_MIN, Q2, SQRT_S, Y_MAX, Y_MIN, X, Y
 from ncteqpy.settings import Settings
