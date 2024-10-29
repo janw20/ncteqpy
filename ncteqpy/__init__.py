0350edfe525ad28a9fa1e71960a018c7278263a3
Jan Wissmann <jan.wissmann.jw@gmail.com>
2024-10-29 11:39:54 +0100

    Set __all__ so mypy doesn't complain

--- ncteqpy/__init__.py
+++ ncteqpy/__init__.py
@@ -1,5 +1,33 @@
 from ncteqpy.chi2 import Chi2
 from ncteqpy.cuts import Cut
 from ncteqpy.data import Dataset, Datasets
-from ncteqpy.kinematic_variables import PT, PT_MAX, PT_MIN, Q2, SQRT_S, Y_MAX, Y_MIN, X, Y
+from ncteqpy.kinematic_variables import (
+    PT,
+    PT_MAX,
+    PT_MIN,
+    Q2,
+    SQRT_S,
+    Y_MAX,
+    Y_MIN,
+    X,
+    Y,
+)
 from ncteqpy.settings import Settings
+
+# otherwise mypy complains with no-implicit-reexport
+__all__ = [
+    "Chi2",
+    "Cut",
+    "Dataset",
+    "Datasets",
+    "PT",
+    "PT_MAX",
+    "PT_MIN",
+    "Q2",
+    "SQRT_S",
+    "Y_MAX",
+    "Y_MIN",
+    "X",
+    "Y",
+    "Settings",
+]
