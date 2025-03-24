c3d1d0c1c07ccdac66d3561a494ca8d1f82a350e
Jan Wissmann <jan.wissmann.jw@gmail.com>
2025-03-24 09:59:27 +0100

    Improve update_kwargs

--- ncteqpy/util.py
+++ ncteqpy/util.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import Any
+from typing import Any, overload
 
 
 def format_unit(variable: str, unit: str) -> str:
@@ -15,9 +15,30 @@ def clamp(x: int, a: int, b: int) -> int:
 
 def update_kwargs(
     kwargs: dict[str, Any],
-    kwargs_user: dict[str, Any] | list[dict[str, Any] | None],
+    kwargs_user: dict[str, Any] | list[dict[str, Any] | None] | None,
     i: int | None = None,
 ) -> dict[str, Any]:
+    """Update `kwargs` with `kwargs_user`. If `kwargs_user` is a list, `i` must be given to select the correct element.
+
+    Parameters
+    ----------
+    kwargs : dict[str, Any]
+        The keyword arguments to update.
+    kwargs_user : dict[str, Any] | list[dict[str, Any]  |  None] | None
+        The keyword arguments to update with.
+    i : int | None, optional
+        Index of `kwargs_user` to use if it is a list, by default None
+
+    Returns
+    -------
+    dict[str, Any]
+        The updated keyword arguments
+
+    Raises
+    ------
+    ValueError
+        If `kwargs_user` is a list and `i` is not given
+    """
     if isinstance(kwargs_user, dict):
         return kwargs | kwargs_user
     elif isinstance(kwargs_user, list):
@@ -28,5 +49,7 @@ def update_kwargs(
                 return kwargs
         else:
             raise ValueError("i must be given if kwargs_user is list")
+    elif kwargs_user is None:
+        return kwargs
     else:
         raise ValueError("kwargs_user must be dict or list")
