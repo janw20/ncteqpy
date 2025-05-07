c819b22ea57055dfe1e39466dcfda456d8039ed8
Jan Wissmann <jan.wissmann.jw@gmail.com>
2025-05-07 16:54:27 +0200

    Improve cuts

--- ncteqpy/cuts.py
+++ ncteqpy/cuts.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import overload, override
 
 import numpy as np
@@ -9,6 +9,68 @@ import numpy.typing as npt
 import pandas as pd
 
 from ncteqpy import data
+
+
+@dataclass
+class Cuts:
+    """Class that represents a collection of cuts."""
+
+    by_type_experiment: dict[str, Cut] = field(default_factory=dict)
+    by_dataset_id: dict[int, Cut] = field(default_factory=dict)
+
+    def get(
+        self, type_experiment: str | None = None, id_dataset: int | None = None
+    ) -> Cut:
+        """Get the cuts corresponding to the given type_experiment and id_dataset
+
+        Parameters
+        ----------
+        type_experiment : str, optional
+            Type of experiment, by default None
+        id_dataset : int, optional
+            Dataset ID, by default None
+
+        Returns
+        -------
+        Cut
+            Cut corresponding to the given type_experiment and id_dataset
+        """
+        cuts = []
+        if type_experiment is not None and type_experiment in self.by_type_experiment:
+            cuts.append(self.by_type_experiment[type_experiment])
+
+        if id_dataset is not None and id_dataset in self.by_dataset_id:
+            cuts.append(self.by_dataset_id[id_dataset])
+
+        if len(cuts) == 0:
+            return Cut_True()
+        elif len(cuts) == 1:
+            return cuts[0]
+        else:
+            return cuts[0] & cuts[1]
+
+    def accept(self, points: pd.DataFrame) -> pd.Series[bool]:
+        """Determine which rows of a DataFrame pass the cuts
+
+        Parameters
+        ----------
+        points : pd.DataFrame
+            DataFrame containing the points to be tested. There must be columns `id_dataset`, `type_experiment` and one for each variable in the cuts
+
+        Returns
+        -------
+        pd.Series[bool]
+            Series of booleans indicating which rows pass the cuts
+        """
+
+        return points.apply(
+                lambda row: self.get(
+                    id_dataset=row["id_dataset"],
+                    type_experiment=row["type_experiment"],
+                ).accepts(row),
+                # apply,
+                axis=1,
+            )
 
 
 class Cuttable(ABC):
@@ -36,20 +98,20 @@ class Cuttable(ABC):
         return Cut_NotEqual(self, value)
 
     @abstractmethod
-    def __hash__(self): ...  # needed so we can make sets of Cuttables
+    def __hash__(self) -> int: ...  # needed so we can make sets of Cuttables
 
 
 class Cut(ABC):
     """Abstract class that represents a cut on a variable."""
 
     @overload
-    def accepts(self, value: float | dict[str, float]) -> bool:
+    def accepts(self, value: float | dict[str, float] | pd.Series[float]) -> bool:
         """Returns True if the value passes the cut.
 
         Parameters
         ----------
         value : float | dict[str, float]
-            Value to be tested. If a float is passed, `value` is inserted for every variable in the cut. If a dictionary is passed, the value corresponding to the variable name (`Cuttable.name`) is inserted
+            Value to be tested. If a float is passed, `value` is checked for every variable in the cut. If a dictionary is passed, the value corresponding to the variable name (`Cuttable.name`) is inserted
 
         Returns
         -------
@@ -60,7 +122,7 @@ class Cut(ABC):
 
     @overload
     def accepts(self, value: npt.NDArray[np.float64]) -> npt.NDArray[np.bool_]:
-        """Check if all values pass the cut. This overload behaves as a vectorized version of calling the `accepts` method with a value of type float.
+        """Determine which values pass the cut. This overload behaves as a vectorized version of calling the `accepts` method with a value of type float.
 
         Parameters
         ----------
@@ -75,18 +137,18 @@ class Cut(ABC):
         ...
 
     @overload
-    def accepts(self, value: pd.Series | pd.DataFrame) -> pd.Series[bool]:
-        """Check if all entries (Series) or rows (DataFrames) pass the cut. If a Series is passed, this overload behaves as a vectorized version of calling the `accepts` method with a value of type float. If a DataFrame is passed, this overload behaves as a vectorized version of calling the `accepts` method with a value of type dict.
+    def accepts(self, value: pd.DataFrame) -> pd.Series[bool]:
+        """Determine which rows pass the cut. `Cuttables` are checked against their corresponding columns.
 
         Parameters
         ----------
-        value : float | dict[str, float] | npt.NDArray[np.float64] | pd.Series | pd.DataFrame
-            Value(s) to be tested.
+        value : pd.DataFrame
+            Values to be tested.
 
         Returns
         -------
         pd.Series[bool]
-            Series of booleans indicating if the values or rows pass the cut.
+            Series of booleans indicating which rows pass the cut.
         """
 
     @abstractmethod
@@ -168,19 +230,20 @@ class Cut_BinLogOp(Cut, ABC):
 
 
 @dataclass(frozen=True)
-class Cut_LessThan(Cut_RelOp):
+class Cut_True(Cut):
+    """Cut that always is True."""
 
-    def __str__(self) -> str:
-        return f"{self.variable} < {self.value}"
-    
+    def __repr__(self) -> str:
+        return "True"
+
     @overload
-    def accepts(self, value: float | dict[str, float]) -> bool: ...
-    
+    def accepts(self, value: float | dict[str, float] | pd.Series[float]) -> bool: ...
+
     @overload
     def accepts(self, value: npt.NDArray[np.float64]) -> npt.NDArray[np.bool_]: ...
 
     @overload
-    def accepts(self, value: pd.Series | pd.DataFrame) -> pd.Series[bool]: ...
+    def accepts(self, value: pd.DataFrame) -> pd.Series[bool]: ...
 
     @override
     def accepts(
@@ -193,8 +256,88 @@ class Cut_LessThan(Cut_RelOp):
             | pd.DataFrame
         ),
     ) -> bool | npt.NDArray[np.bool_] | pd.Series[bool]:
-        if isinstance(value, (dict, pd.DataFrame)):
-            return value[self.variable.name] < self.value
+        if isinstance(value, np.ndarray):
+            return np.full(value.shape, True)
+        elif isinstance(value, pd.DataFrame):
+            return pd.Series(value.shape[0] * [True])
+        else:
+            return True
+
+    @override
+    def variables(self) -> set[Cuttable]:
+        return set()
+
+
+@dataclass(frozen=True)
+class Cut_False(Cut):
+    """Cut that always is False."""
+
+    def __repr__(self) -> str:
+        return "False"
+
+    @overload
+    def accepts(self, value: float | dict[str, float] | pd.Series[float]) -> bool: ...
+
+    @overload
+    def accepts(self, value: npt.NDArray[np.float64]) -> npt.NDArray[np.bool_]: ...
+
+    @overload
+    def accepts(self, value: pd.DataFrame) -> pd.Series[bool]: ...
+
+    @override
+    def accepts(
+        self,
+        value: (
+            float
+            | npt.NDArray[np.float64]
+            | dict[str, float]
+            | pd.Series
+            | pd.DataFrame
+        ),
+    ) -> bool | npt.NDArray[np.bool_] | pd.Series[bool]:
+        if isinstance(value, np.ndarray):
+            return np.full(value.shape, False)
+        elif isinstance(value, pd.DataFrame):
+            return pd.Series(value.shape[0] * [False])
+        else:
+            return False
+
+
+@dataclass(frozen=True)
+class Cut_LessThan(Cut_RelOp):
+
+    def __repr__(self) -> str:
+        return f"{self.variable} < {self.value}"
+
+    @overload
+    def accepts(self, value: float | dict[str, float] | pd.Series[float]) -> bool: ...
+
+    @overload
+    def accepts(self, value: npt.NDArray[np.float64]) -> npt.NDArray[np.bool_]: ...
+
+    @overload
+    def accepts(self, value: pd.DataFrame) -> pd.Series[bool]: ...
+
+    @override
+    def accepts(
+        self,
+        value: (
+            float
+            | npt.NDArray[np.float64]
+            | dict[str, float]
+            | pd.Series
+            | pd.DataFrame
+        ),
+    ) -> bool | npt.NDArray[np.bool_] | pd.Series[bool] | pd.DataFrame:
+        if isinstance(value, (dict, pd.Series, pd.DataFrame)):
+            if self.variable.name in value:
+                return value[self.variable.name] < self.value
+            else:
+                return (
+                    pd.Series(value.shape[0] * [True])
+                    if isinstance(value, pd.DataFrame)
+                    else True
+                )
         else:
             return value < self.value
 
@@ -202,17 +345,17 @@ class Cut_LessThan(Cut_RelOp):
 @dataclass(frozen=True)
 class Cut_LessThanEqual(Cut_RelOp):
 
-    def __str__(self) -> str:
+    def __repr__(self) -> str:
         return f"{self.variable} <= {self.value}"
-    
+
     @overload
-    def accepts(self, value: float | dict[str, float]) -> bool: ...
-    
+    def accepts(self, value: float | dict[str, float] | pd.Series[float]) -> bool: ...
+
     @overload
     def accepts(self, value: npt.NDArray[np.float64]) -> npt.NDArray[np.bool_]: ...
 
     @overload
-    def accepts(self, value: pd.Series | pd.DataFrame) -> pd.Series[bool]: ...
+    def accepts(self, value: pd.DataFrame) -> pd.Series[bool]: ...
 
     @override
     def accepts(
@@ -225,7 +368,7 @@ class Cut_LessThanEqual(Cut_RelOp):
             | pd.DataFrame
         ),
     ) -> bool | npt.NDArray[np.bool_] | pd.Series[bool]:
-        if isinstance(value, (dict, pd.DataFrame)):
+        if isinstance(value, (dict, pd.Series, pd.DataFrame)):
             return value[self.variable.name] <= self.value
         else:
             return value <= self.value
@@ -234,17 +377,17 @@ class Cut_LessThanEqual(Cut_RelOp):
 @dataclass(frozen=True)
 class Cut_GreaterThan(Cut_RelOp):
 
-    def __str__(self) -> str:
+    def __repr__(self) -> str:
         return f"{self.variable} > {self.value}"
-    
+
     @overload
-    def accepts(self, value: float | dict[str, float]) -> bool: ...
-    
+    def accepts(self, value: float | dict[str, float] | pd.Series[float]) -> bool: ...
+
     @overload
     def accepts(self, value: npt.NDArray[np.float64]) -> npt.NDArray[np.bool_]: ...
 
     @overload
-    def accepts(self, value: pd.Series | pd.DataFrame) -> pd.Series[bool]: ...
+    def accepts(self, value: pd.DataFrame) -> pd.Series[bool]: ...
 
     @override
     def accepts(
@@ -257,7 +400,7 @@ class Cut_GreaterThan(Cut_RelOp):
             | pd.DataFrame
         ),
     ) -> bool | npt.NDArray[np.bool_] | pd.Series[bool]:
-        if isinstance(value, (dict, pd.DataFrame)):
+        if isinstance(value, (dict, pd.Series, pd.DataFrame)):
             return value[self.variable.name] > self.value
         else:
             return value > self.value
@@ -266,17 +409,17 @@ class Cut_GreaterThan(Cut_RelOp):
 @dataclass(frozen=True)
 class Cut_GreaterThanEqual(Cut_RelOp):
 
-    def __str__(self) -> str:
+    def __repr__(self) -> str:
         return f"{self.variable} >= {self.value}"
-    
+
     @overload
-    def accepts(self, value: float | dict[str, float]) -> bool: ...
-    
+    def accepts(self, value: float | dict[str, float] | pd.Series[float]) -> bool: ...
+
     @overload
     def accepts(self, value: npt.NDArray[np.float64]) -> npt.NDArray[np.bool_]: ...
 
     @overload
-    def accepts(self, value: pd.Series | pd.DataFrame) -> pd.Series[bool]: ...
+    def accepts(self, value: pd.DataFrame) -> pd.Series[bool]: ...
 
     @override
     def accepts(
@@ -289,7 +432,7 @@ class Cut_GreaterThanEqual(Cut_RelOp):
             | pd.DataFrame
         ),
     ) -> bool | npt.NDArray[np.bool_] | pd.Series[bool]:
-        if isinstance(value, (dict, pd.DataFrame)):
+        if isinstance(value, (dict, pd.Series, pd.DataFrame)):
             return value[self.variable.name] >= self.value
         else:
             return value >= self.value
@@ -298,17 +441,17 @@ class Cut_GreaterThanEqual(Cut_RelOp):
 @dataclass(frozen=True)
 class Cut_Equal(Cut_RelOp):
 
-    def __str__(self) -> str:
+    def __repr__(self) -> str:
         return f"{self.variable} == {self.value}"
-    
+
     @overload
-    def accepts(self, value: float | dict[str, float]) -> bool: ...
-    
+    def accepts(self, value: float | dict[str, float] | pd.Series[float]) -> bool: ...
+
     @overload
     def accepts(self, value: npt.NDArray[np.float64]) -> npt.NDArray[np.bool_]: ...
 
     @overload
-    def accepts(self, value: pd.Series | pd.DataFrame) -> pd.Series[bool]: ...
+    def accepts(self, value: pd.DataFrame) -> pd.Series[bool]: ...
 
     @override
     def accepts(
@@ -321,7 +464,7 @@ class Cut_Equal(Cut_RelOp):
             | pd.DataFrame
         ),
     ) -> bool | npt.NDArray[np.bool_] | pd.Series[bool]:
-        if isinstance(value, (dict, pd.DataFrame)):
+        if isinstance(value, (dict, pd.Series, pd.DataFrame)):
             return value[self.variable.name] == self.value
         else:
             return value == self.value
@@ -330,17 +473,17 @@ class Cut_Equal(Cut_RelOp):
 @dataclass(frozen=True)
 class Cut_NotEqual(Cut_RelOp):
 
-    def __str__(self) -> str:
+    def __repr__(self) -> str:
         return f"{self.variable} != {self.value}"
-    
+
     @overload
-    def accepts(self, value: float | dict[str, float]) -> bool: ...
-    
+    def accepts(self, value: float | dict[str, float] | pd.Series[float]) -> bool: ...
+
     @overload
     def accepts(self, value: npt.NDArray[np.float64]) -> npt.NDArray[np.bool_]: ...
 
     @overload
-    def accepts(self, value: pd.Series | pd.DataFrame) -> pd.Series[bool]: ...
+    def accepts(self, value: pd.DataFrame) -> pd.Series[bool]: ...
 
     @override
     def accepts(
@@ -353,7 +496,7 @@ class Cut_NotEqual(Cut_RelOp):
             | pd.DataFrame
         ),
     ) -> bool | npt.NDArray[np.bool_] | pd.Series[bool]:
-        if isinstance(value, (dict, pd.DataFrame)):
+        if isinstance(value, (dict, pd.Series, pd.DataFrame)):
             return value[self.variable.name] != self.value
         else:
             return value != self.value
@@ -362,22 +505,29 @@ class Cut_NotEqual(Cut_RelOp):
 @dataclass(frozen=True)
 class Cut_And(Cut_BinLogOp):
 
-    def __str__(self) -> str:
+    def __repr__(self) -> str:
         return f"({self.cut1}) & ({self.cut2})"
-    
+
     @overload
-    def accepts(self, value: float | dict[str, float]) -> bool: ...
-    
+    def accepts(self, value: float | dict[str, float] | pd.Series[float]) -> bool: ...
+
     @overload
     def accepts(self, value: npt.NDArray[np.float64]) -> npt.NDArray[np.bool_]: ...
 
     @overload
-    def accepts(self, value: pd.Series | pd.DataFrame) -> pd.Series[bool]: ...
+    def accepts(self, value: pd.DataFrame) -> pd.Series[bool]: ...
 
     @override
     def accepts(
-        self, value: float | npt.NDArray[np.float64]
-    ) -> bool | npt.NDArray[np.bool_]:
+        self,
+        value: (
+            float
+            | npt.NDArray[np.float64]
+            | dict[str, float]
+            | pd.Series
+            | pd.DataFrame
+        ),
+    ) -> bool | npt.NDArray[np.bool_] | pd.Series[bool]:
         if isinstance(value, (np.ndarray, pd.Series, pd.DataFrame)):
             return self.cut1.accepts(value) & self.cut2.accepts(value)
         else:
@@ -387,17 +537,17 @@ class Cut_And(Cut_BinLogOp):
 @dataclass(frozen=True)
 class Cut_Or(Cut_BinLogOp):
 
-    def __str__(self) -> str:
+    def __repr__(self) -> str:
         return f"({self.cut1}) | ({self.cut2})"
-    
+
     @overload
-    def accepts(self, value: float | dict[str, float]) -> bool: ...
-    
+    def accepts(self, value: float | dict[str, float] | pd.Series[float]) -> bool: ...
+
     @overload
     def accepts(self, value: npt.NDArray[np.float64]) -> npt.NDArray[np.bool_]: ...
 
     @overload
-    def accepts(self, value: pd.Series | pd.DataFrame) -> pd.Series[bool]: ...
+    def accepts(self, value: pd.DataFrame) -> pd.Series[bool]: ...
 
     @override
     def accepts(
@@ -419,45 +569,59 @@ class Cut_Or(Cut_BinLogOp):
 @dataclass(frozen=True)
 class Cut_Xor(Cut_BinLogOp):
 
-    def __str__(self) -> str:
+    def __repr__(self) -> str:
         return f"({self.cut1}) ^ ({self.cut2})"
-    
+
     @overload
-    def accepts(self, value: float | dict[str, float]) -> bool: ...
-    
+    def accepts(self, value: float | dict[str, float] | pd.Series[float]) -> bool: ...
+
     @overload
     def accepts(self, value: npt.NDArray[np.float64]) -> npt.NDArray[np.bool_]: ...
 
     @overload
-    def accepts(self, value: pd.Series | pd.DataFrame) -> pd.Series[bool]: ...
+    def accepts(self, value: pd.DataFrame) -> pd.Series[bool]: ...
 
     @override
     def accepts(
-        self, value: float | npt.NDArray[np.float64]
-    ) -> bool | npt.NDArray[np.bool_]:
+        self,
+        value: (
+            float
+            | npt.NDArray[np.float64]
+            | dict[str, float]
+            | pd.Series
+            | pd.DataFrame
+        ),
+    ) -> bool | npt.NDArray[np.bool_] | pd.Series[bool]:
         return self.cut1.accepts(value) != self.cut2.accepts(value)
 
 
 @dataclass(frozen=True)
 class Cut_Not(Cut_UnLogOp):
 
-    def __str__(self) -> str:
+    def __repr__(self) -> str:
         return f"~({self.cut})"
-    
+
     @overload
-    def accepts(self, value: float | dict[str, float]) -> bool: ...
-    
+    def accepts(self, value: float | dict[str, float] | pd.Series[float]) -> bool: ...
+
     @overload
     def accepts(self, value: npt.NDArray[np.float64]) -> npt.NDArray[np.bool_]: ...
 
     @overload
-    def accepts(self, value: pd.Series | pd.DataFrame) -> pd.Series[bool]: ...
+    def accepts(self, value: pd.DataFrame) -> pd.Series[bool]: ...
 
     @override
     def accepts(
-        self, value: float | npt.NDArray[np.float64]
-    ) -> bool | npt.NDArray[np.bool_]:
-        if isinstance(value, np.ndarray):
+        self,
+        value: (
+            float
+            | npt.NDArray[np.float64]
+            | dict[str, float]
+            | pd.Series
+            | pd.DataFrame
+        ),
+    ) -> bool | npt.NDArray[np.bool_] | pd.Series[bool]:
+        if isinstance(value, (np.ndarray, pd.DataFrame)):
             return ~self.cut.accepts(value)
         else:
             return not self.cut.accepts(value)
