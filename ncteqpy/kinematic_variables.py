1a5bff3b44fb7af860cc82c616475c8faa421167
Jan Wissmann <jan.wissmann.jw@gmail.com>
2025-05-23 10:48:16 +0200

    Add W2

--- ncteqpy/kinematic_variables.py
+++ ncteqpy/kinematic_variables.py
@@ -18,4 +18,18 @@ Y_MIN = KinematicVariable("y_min")
 Y_MAX = KinematicVariable("y_max")
 X = KinematicVariable("x")
 Q2 = KinematicVariable("Q2")
+W2 = KinematicVariable("W2")
 SQRT_S = KinematicVariable("sqrt_s")
+
+label_to_kinvar: dict[str, KinematicVariable] = {
+    "pT": PT,
+    "pT_min": PT_MIN,
+    "pT_max": PT_MAX,
+    "y": Y,
+    "y_min": Y_MIN,
+    "y_max": Y_MAX,
+    "x": X,
+    "Q2": Q2,
+    "W2": W2,
+    "sqrt_s": SQRT_S,
+}
