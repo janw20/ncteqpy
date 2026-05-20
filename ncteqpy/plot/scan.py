63bfc6e61ff3bfa294b6f0da022bf4f0fffaeb09
Jan Wissmann <jan.wissmann.jw@gmail.com>
2026-05-20 11:00:44 +0200

    Re-add plot_scan_2d (whoops)

--- ncteqpy/plot/scan.py
+++ ncteqpy/plot/scan.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
 
+from math import sqrt
+
+import matplotlib.colors as mcolors
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -249,3 +252,103 @@ def plot_scan_1d(
             kwargs_legend_default = {}
             kwargs_legend_updated = update_kwargs(kwargs_legend_default, kwargs_legend)
             ax_i.legend(**kwargs_legend_updated)
+
+
+def plot_scan_2d(
+    ax: plt.Axes | Sequence[plt.Axes],
+    profile_params: pd.DataFrame,
+    minimum: pd.DataFrame,
+    profile_chi2: pd.DataFrame,
+    parameters: tuple[str, str] | list[tuple[str, str]] | None = None,
+    tolerance: float | None = None,
+    **kwargs: Any,
+) -> None:
+
+    if parameters is None:
+        parameters = list(
+            zip(
+                profile_params.columns.get_level_values(0),
+                profile_params.columns.get_level_values(1),
+            )
+        )
+    elif isinstance(parameters, tuple):
+        parameters = [parameters]
+
+    if isinstance(ax, plt.Axes):
+        ax = [ax]
+    elif len(ax) != len(parameters):
+        raise ValueError(
+            "The number of axes must be equal to the number of parameter pairs."
+        )
+
+    if len(profile_params) != len(profile_chi2):
+        raise ValueError(
+            "The number of parameter points must be equal to the number of chi2 values."
+        )
+
+    n = int(sqrt(len(profile_params)))
+    if n**2 != len(profile_params):
+        raise ValueError("Only scans on square grids are supported.")
+
+    for p, ax_i in zip(parameters, ax):
+
+        ax_i.set_adjustable("box")
+        ax_i.set_box_aspect(1)
+
+        norm = mcolors.TwoSlopeNorm(tolerance) if tolerance is not None else None
+
+        image = ax_i.imshow(
+            np.reshape(profile_chi2[p] - minimum["chi2"].iloc[0], (n, n)),
+            extent=(
+                profile_params[*p, 0].min(),
+                profile_params[*p, 0].max(),
+                profile_params[*p, 1].min(),
+                profile_params[*p, 1].max(),
+            ),
+            cmap="Spectral_r",
+            norm=norm,  # pyright: ignore[reportArgumentType]
+            interpolation="bicubic",
+            origin="lower",
+            aspect="auto",
+            **kwargs,
+        )
+
+        cb = ax_i.figure.colorbar(
+            image,
+            ax=ax_i,
+        )
+        cb.set_label(r"$\Delta \chi^2$")
+        cb.ax.set_yscale("linear")
+
+        if tolerance is not None:
+            c = ax_i.contour(
+                np.reshape(profile_params[*p, 0], (n, n)),
+                np.reshape(profile_params[*p, 1], (n, n)),
+                np.reshape(profile_chi2[p] - minimum["chi2"].iloc[0], (n, n)),
+                levels=[tolerance / 2, tolerance, 2 * tolerance],
+                colors="black",
+            )
+            ax_i.clabel(c, c.levels)  # pyright: ignore[reportAttributeAccessIssue]
+
+        ax_i.plot(minimum[p[0]], minimum[p[1]], "*", color="black")
+
+        ax_i.set(
+            xlabel=f"${parameters_cj15_py_to_tex[p[0]]}$",
+            ylabel=f"${parameters_cj15_py_to_tex[p[1]]}$",
+        )
+
+        # fix for overlapping ticks
+        ax_i.ticklabel_format(
+            axis="both",
+            style="sci",
+            scilimits=(
+                -2,
+                ax_i.xaxis.major.formatter._powerlimits[  # pyright: ignore[reportAttributeAccessIssue]
+                    1
+                ],
+            ),
+        )
+        # alternative fix for overlapping ticks
+        # ax.xaxis.set_tick_params(rotation=20)
+
+        ax_i.grid()
